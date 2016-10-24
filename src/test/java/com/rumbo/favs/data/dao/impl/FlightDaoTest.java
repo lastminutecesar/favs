a8be66ec8af7a72f3b871178276d2cecc604bdf1
Cesar <Cesar@Cesar-Portatil>
2016-10-24 23:23:20 +0200

    REFACTORING

---
+++ src/test/java/com/rumbo/favs/data/dao/impl/FlightDaoTest.java
@@ -0,0 +1,43 @@
+package com.rumbo.favs.data.dao.impl;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.rumbo.favs.business.services.core.impl.SearchEngineImpl;
+import com.rumbo.favs.data.dao.IFlightDao;
+import com.rumbo.favs.data.dao.IInfantPriceDao;
+import com.rumbo.favs.data.entities.Flight;
+import com.rumbo.favs.data.entities.InfantPrice;
+
+public class FlightDaoTest {
+	
+	private static IFlightDao flightDao;
+	
+	@BeforeClass
+	public static void initialize()
+	{
+		flightDao = new FlightDaoImpl();
+	}
+
+	@Test		
+	public void testA() {
+		
+		List<Flight> flights = flightDao.getFlightsByItinerary("AMS", "FRA");
+		
+		assertThat(flights.size(),is(3));
+	}
+	
+	@Test		
+	public void testB() {
+		
+		List<Flight> flights = flightDao.getFlightsByItinerary("LHR", "IST");
+		
+		assertThat(flights.size(),is(2));
+	}
+	
+}
